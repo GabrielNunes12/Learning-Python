dd3a4e532f67da3a9a18c7d8f6433508598bf9bd
Gabriel <prismaticnetwork12@gmail.com>
2022-06-03 10:29:45 -0300

    feat: adding more exercises

--- CursoEmVideo/Aula02/Aula02.py
+++ CursoEmVideo/Aula02/Aula02.py
@@ -16,3 +16,26 @@ print(f'Seu numero: {yourNumber}');
 print(f'Dobro: {doubleNumber}');
 print(f'triplo: {tripleNumber}');
 print(f'Raiz quadrada: {format(squareRoot,".2f")}');
+
+## Average grade
+firstGrade = float(input('Digite sua primeira nota: '));
+secondGrade = float(input('Digite a segunda nota: '));
+result = (firstGrade + secondGrade) / 2;
+print(f'A primeira nota: {firstGrade} e sua segunda foi: {secondGrade}, resultando: {result}');
+
+## converting meters
+meters = float(input('Digite a medida em metros: '));
+dam = meters / 10;
+hm = dam / 10;
+km = hm / 10;
+dm = meters * 10;
+cm = dm * 10;
+mm = cm * 10;
+
+print(f'A medida de: {meters}m corresponde a ');
+print(f'Km: {km} km');
+print(f'Hm: {hm} hm');
+print(f'Dam: {dam} dam');
+print(f'dm: {dm} dm');
+print(f'cm: {cm} cm');
+print(f'Mm: {mm} mm');
